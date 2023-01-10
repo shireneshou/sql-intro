d56f0ed74943d99dfbadb4174a00a323ad264de7
shireneshou <shirene.shoushtarizadeh@kellogg.northwestern.edu>
2023-01-10 22:22:59 +0000

    End of Class 2

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,4 +1,9 @@
 -- How many lifetime hits does Barry Bonds have?
+SELECT SUM(stats.hits) 
+FROM players
+INNER JOIN stats ON stats.player_id = players.id
+WHERE first_name = "Barry"
+AND last_name = "Bonds";
 
 -- Expected result:
 -- 2935
